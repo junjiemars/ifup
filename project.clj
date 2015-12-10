b37b0034b443c853bda49dbb83d3b35de50fef66
南山 <junjiemars@gmail.com>
2015-12-11 00:22:13 +0800

    return the client ip via x-forwarded-for http header

--- project.clj
+++ project.clj
@@ -4,11 +4,13 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [org.clojure/tools.logging "0.3.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler ifup.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}
    :uberwar {:aot :all}}
+
   :javac-options ["-target" "1.6" "-source" "1.8"])
--- src/ifup/handler.clj
+++ src/ifup/handler.clj
@@ -1,11 +1,17 @@
 (ns ifup.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clojure.tools.logging :as l]
+            [clojure.string :as s]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (GET "/" {:keys [remote-addr headers] :as request}
+       (let [x-forwarded-for (get headers "x-forwarded-for")]
+         (if (empty? x-forwarded-for)
+           remote-addr
+           (first (s/split x-forwarded-for #",")))))
+  (route/not-found "Hello World"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
